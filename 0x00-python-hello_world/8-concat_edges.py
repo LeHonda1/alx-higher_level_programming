bac8dca07f63d8dca216e08f46a2eab648f582d5
LeHonda1 <124697668+LeHonda1@users.noreply.github.com>
2023-05-10 11:10:08 +0300

    Update 8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-str1 = "object-oriented"
-str2 = " programming"
-str3 = " with "
-str4 = "Python"
-
-print(str1 +" "+ str3.strip()+" " + str2.strip() + " " + str4.strip())
+str1 = "object-oriented" str2 = " programming" str3 = " with " str4 = "Python"
+print(str1 +" "+ str2.strip()+" " + str3.strip() + " " + str4.strip())
